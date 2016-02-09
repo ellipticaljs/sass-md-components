ef0d96cdc3c0650ec3c7ce80e3e30f77c98c22bd
Scott <sfrancis@misinteractive.com>
2016-02-08 22:56:34 -0500

    added bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 
 var gulp=require('gulp');
 var sassdoc = require('sassdoc');
+var sass = require('gulp-sass');
+var concat=require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
 
 gulp.task('default',function(){
     console.log('echo sass md components...');
@@ -11,3 +15,25 @@ gulp.task('sassdoc', function () {
         .pipe(sassdoc());
 
 });
+
+gulp.task('dist', function () {
+    compileSass();
+});
+
+gulp.task('minify',function(){
+    concatStream('./dist/md.components.css','md.components.min.css')
+        .pipe(minify())
+        .pipe(gulp.dest('./dist'));
+});
+
+function compileSass(){
+    gulp.src('./src/md.components.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist'));
+}
+
+function concatStream(src,name){
+    return gulp.src(src)
+        .pipe(concat(name))
+
+}
